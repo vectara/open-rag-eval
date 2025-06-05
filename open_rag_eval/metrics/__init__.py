1d4176b97adf10196d57ec170a37630e0b4ccf97
Vishal Naik <naik.vishalishwar@gmail.com>
2025-06-05 11:43:09 -0700

    Add ConsistencyEvaluator with BERT and ROUGE-based similarity metrics

--- open_rag_eval/metrics/__init__.py
+++ open_rag_eval/metrics/__init__.py
@@ -3,8 +3,12 @@ from .citation_metric import CitationMetric
 from .no_answer_metric import NoAnswerMetric
 from .umbrela_metric import UMBRELAMetric
 from .hallucination_metric import HallucinationMetric
-from .base_metrics import AugmentedGenerationMetric, RetrievalMetric
+from .base_metrics import AugmentedGenerationMetric, RetrievalMetric, PairwiseAnswerSimilarityMetric
+from .bert_score_similarity_metric import BERTScoreSimilarityMetric
+from .rouge_score_similarity_metric import ROUGEScoreSimilarityMetric
+
 
 __all__ = ["AutoNuggetMetric", "CitationMetric", "UMBRELAMetric",
            "AugmentedGenerationMetric", "NoAnswerMetric", "RetrievalMetric",
-           "HallucinationMetric"]
+           "HallucinationMetric", "PairwiseAnswerSimilarityMetric", "BERTScoreSimilarityMetric",
+           "ROUGEScoreSimilarityMetric"]
--- open_rag_eval/utils/constants.py
+++ open_rag_eval/utils/constants.py
@@ -1,3 +1,5 @@
 # open_rag_eval/utils/constants.py
 NO_ANSWER = "NO_ANSWER"
 API_ERROR = "API_ERROR"
+BERT_SCORE = "bert_score"
+ROUGE_SCORE = "rouge_score"
