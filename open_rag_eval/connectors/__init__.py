f9038ce496c44e32be1999678496aad451023647
ALAPATI DEVI ANUSHA <alapatidevianusha@gmail.com>
2026-04-04 16:59:30 +0530

    Add ChromaDB connector for evaluating existing ChromaDB collections

--- open_rag_eval/connectors/__init__.py
+++ open_rag_eval/connectors/__init__.py
@@ -2,5 +2,12 @@ from .connector import Connector
 from .vectara_connector import VectaraConnector
 from .llama_index_connector import LlamaIndexConnector
 from .langchain_connector import LangChainConnector
+from .chromadb_connector import ChromaDBConnector
 
-__all__ = ["Connector", "VectaraConnector", "LlamaIndexConnector", "LangChainConnector"]
+__all__ = [
+    "Connector",
+    "VectaraConnector",
+    "LlamaIndexConnector",
+    "LangChainConnector",
+    "ChromaDBConnector",
+]
